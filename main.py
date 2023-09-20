9ef5c8d17b3bca264f31c71a882c7427e6810188
AlisaPh <a.arthopkins@gmail.com>
2023-09-20 16:58:57 +0300

    hey yo

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def drawGrid(surface):
 
 def main():
     pygame.init()
-    menu.Button
+
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((consts.WIDTH, consts.HEIGHT), 0, 32)
 
