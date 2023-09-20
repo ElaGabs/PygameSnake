b6ff37972a7d6400cee7da1a3c2c09b9e392af42
AlisaPh <a.arthopkins@gmail.com>
2023-09-20 11:45:49 +0300

    hey yo

--- ImageEdit.py
+++ ImageEdit.py
@@ -14,12 +14,6 @@ def draw_train(scrn):
 
 
 # npc
-# def draw_npc(scrn):
-#     npc = {"x_val": 240, "y_val": 240}
-#     consts.TRAIN = pygame.transform.scale(consts.TRAIN, (100, 100))
-#     scrn.blit(consts.TRAIN, (240, 240))
-#     pygame.display.flip()
-#     return train
 
 def add_to_npc_list():
     npc_list = [consts.npc1, consts.npc2, consts.npc3, consts.npc4, consts.npc5, consts.npc6, consts.npc7, consts.npc8, consts.npc9, consts.npc10, consts.npc11,consts.npc12]
