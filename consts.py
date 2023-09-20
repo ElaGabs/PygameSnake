fa0ea92592633b473d80d8e5b38947e063c73850
AlisaPh <a.arthopkins@gmail.com>
2023-09-20 09:48:02 +0300

    hey yo

---
+++ consts.py
@@ -0,0 +1,14 @@
+import pygame
+
+SCREEN_LENGTH = 1000
+SCREEN_WIDTH = 1000
+
+NPC = pygame.image.load("npc.png")
+TRAIN = pygame.image.load("train.png")
+
+TRAIN_HEIGHT = 200
+TRAIN_WIDTH = 200
+
+NPC_HEIGHT = 50
+NPC_WIDTH = 50
+
